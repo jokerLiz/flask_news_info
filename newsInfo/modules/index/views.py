70d22b8a5c0f0ff9a8f7e6b39ac81a627a6e1ce4
jokerLiz <59729729+jokerLiz@users.noreply.github.com>
2020-06-17 12:48:59 +0800

    测试数据，index的ico访问

--- newsInfo/modules/index/views.py
+++ newsInfo/modules/index/views.py
@@ -1,10 +1,16 @@
 #视图
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blue      #导入__init__.py下的蓝图对象index_blue
 
 
-#为该蓝图编写视图函数
+'''为该蓝图编写视图函数'''
+#首页
 @index_blue.route('/')
 def index():
     return render_template('news/index.html')
+
+#网页标签logo
+@index_blue.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
